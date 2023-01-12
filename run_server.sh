7ab23377b48ddede660f96c247bc755db86f4853
rhoang <raymond.hoang@water.ca.gov>
2023-01-12 13:25:22 -0800

    remove comments

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,1 @@
-# assume that the container already has activated the right environment
-#panel serve dashboard.py --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
 panel serve test_serve.py --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
